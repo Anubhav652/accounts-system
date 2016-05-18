ba5d1e304fd5bb73a0500431dcd69c4b21cd85dc
Anubhav652 <anubhavagarwal033@gmail.com>
2016-05-18 21:36:27 +0800

    Settings.php

---
+++ Settings.php
@@ -0,0 +1,6 @@
+<?php
+	$servername = "localhost"; // It is basically the host name/IP address you need to connect to
+	$username = "root"; // MySQL username should be typed here
+	$password = ""; // MySQL password should be typed here
+	$database = "accounts"; // MySQL database which you are going to use, should be typed here!
+?>
